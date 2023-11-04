20e69882029fe48486a591f6dbb872dccb8effd9
Henrik Albers <halbers@mail.uni-paderborn.de>
2023-11-04 15:35:33 +0200

    Add vba class with initialization

--- Assignment/SmartMeter.py
+++ Assignment/SmartMeter.py
@@ -10,36 +10,39 @@ class SmartMeter:
     Single Smart Meter
 
     Args:
-        M1(List[int]): last t records of consumption
+        M1(np.ndarray): last t records of consumption
     """
 
-    def __init__(self, M1: List[int]) -> None:
+    def __init__(self, M1: np.ndarray, attack_status: str) -> None:
         self.M1 = M1
+        self.attack_status = attack_status
         self.T = len(M1)
         self.Y = random.sample(range(100), self.T)
         self.own_mean = statistics.mean(M1)
         self.own_proba_hist = self.calculate_proba_hist(M1)
         self.own_entropy = self.calculate_entropy(M1)
 
-    def calculate_proba_hist(self, elems: List[int]):
+    def calculate_proba_hist(self, elems: np.ndarray):
         """
         Calculates probability distribution from given histogram
 
         Args:
-            elems(List[int]): Elements over which the probability is calculated
+            elems(np.ndarray): Elements over which the probability is calculated
 
         Returns:
-            List[int]: Returns a list containing the probabilities of each element
+            np.ndarray: Returns a list containing the probabilities of each element
         """
-        probabilities = [elems.count(x)/len(elems) for x in elems]
+        # TODO: Currently wrong, has to be updated according to
+        #       https://www.scribbr.com/statistics/probability-distributions/
+        probabilities = [len((elems == x))/len(elems) for x in elems]
         return probabilities
 
-    def calculate_entropy(self, elems: List[int]):
+    def calculate_entropy(self, elems: np.ndarray):
         """
         Calculates entropy based on the formula given in the paper
 
         Args:
-            elems(List[int]): Elements over which the entropy is calculated
+            elems(np.ndarray): Elements over which the entropy is calculated
 
         Returns:
             int: Entropy of the given list
--- Assignment/VBA.py
+++ Assignment/VBA.py
@@ -1,3 +1,25 @@
+import numpy as np
+
+from data.generate_test_data import generate_test_data_df
+from SmartMeter import SmartMeter
+
 
 class VBA:
-    pass
+    def __init__(self):
+        data = generate_test_data_df(T=5, N=10)
+        data = np.array(data)
+        smart_meters = []
+        for record in data:
+            smart_meters.append(
+                SmartMeter(
+                    M1=record[:-1],
+                    attack_status=record[-1]
+                )
+            )
+        self.smart_meters = smart_meters
+
+
+if __name__ == "__main__":
+    vba = VBA()
+    print(vba.smart_meters[0].M1)
+    print(vba.smart_meters[0].own_entropy)
