4dd3ff1bef97d711ff97b430f83df6241789f8b5
Henrik Albers <halbers@mail.uni-paderborn.de>
2023-11-13 12:26:36 +0200

    final implementation of u

--- Assignment/SmartMeterHBA.py
+++ Assignment/SmartMeterHBA.py
@@ -1,6 +1,11 @@
 import numpy as np
 from typing import List
 
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from VirtualCollectorHBA import VirtualCollectorHBA
+
 class SmartMeterHBA:
     def __init__(
         self,
@@ -31,6 +36,43 @@ class SmartMeterHBA:
         """
         return self._historic_data[:t]
     
+    def update_ptj(self, ptj:np.ndarray):
+        self.ptj = ptj
+
+    def dist_lu_decomp(self, v_col: "VirtualCollectorHBA", j: int, Pt=None, previous_l: np.ndarray = None, previous_u: np.ndarray = None):
+        if j == -1:
+            # SM is first SM of virtual collector
+            # TODO: No it is the collectors SM of LU paper
+            Pt = self.ptj
+            v_col.lu_decomp_next(j, Pt)
+        else:
+            v_col_len = len(v_col.sm_sample)
+            if j < v_col_len:
+                # compute u
+                # for q in range(1, j):
+                u = self.compute_u(j, previous_l, v_col_len)
+                print(u)
+                # compute l
+
+                # compute y
+
+                # transmit data and notify next
+                v_col.lu_decomp_next(j, Pt, previous_u=u)
+            else:
+                # SM is last SM of virtual collector
+                pass
+
+    def compute_u(self, j:int, previous_l: np.ndarray, previous_u: np.ndarray, v_col_len: int):
+        # TODO: Complain about privacy concerns. Normal LU uses SM0 (Collector) to mask consumption of SM1.
+        #       The new algorithm lets SM1 send the data plainly to SM2
+        if j == 0:
+            return self._historic_data
+        hist_data = self._historic_data
+        first_elem = hist_data[0]
+        u = []
+        for i in range(0, v_col_len):
+            u.append(first_elem - previous_l[i,j-1] * previous_u[i-1,j])
+        return np.array(u)
 
 def generate_smart_meters_HBA(
     data: np.ndarray
--- Assignment/VirtualCollectorHBA.py
+++ Assignment/VirtualCollectorHBA.py
@@ -31,9 +31,21 @@ class VirtualCollectorHBA:
         # Calculate P~
         nl = []
         for col_idx in self.sm_belongs_to_col_of_index:
-            nl.append([self.collectors[col_idx].sm_count for x in range(t)])
+            nl.append([self.collectors[col_idx].sm_count for _ in range(t)])
         nml = [[self.sm_belongs_to_col_of_index.count(x) for _ in range(t)] for x in self.sm_belongs_to_col_of_index]
         P = np.multiply(np.divide(submitted_consumption, nl), nml)
 
         # Start communication (also send index of sm in swarm)
-        pass
+        self.broadcast_ptj(P) 
+        self.sm_sample[0].dist_lu_decomp(self, -1)
+
+
+    def broadcast_ptj(self, P=np.ndarray):
+        for idx, sm in enumerate(self.sm_sample):
+            # TODO: ptj is probably not P: Probably its the sum of the values of the row
+            row_sum = np.sum(P[idx, :])
+            sm.update_ptj(row_sum)
+
+    def lu_decomp_next(self, idx: int, Pt: float):
+        if idx < len(self.sm_sample)-1:
+            self.sm_sample[idx+1].dist_lu_decomp(self, idx+1, Pt)
